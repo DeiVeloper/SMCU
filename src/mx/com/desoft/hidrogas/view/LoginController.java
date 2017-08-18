38bf6e68a18aa201466c2c9e935b459568df480e
DeiVeloper <david.c13@hotmail.com>
2017-08-18 17:52:53 -0500

    Se agrega evento para el boton de iniciar sesión

--- src/mx/com/desoft/hidrogas/view/LoginController.java
+++ src/mx/com/desoft/hidrogas/view/LoginController.java
@@ -1,31 +1,39 @@
 package mx.com.desoft.hidrogas.view;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import mx.com.desoft.hidrogas.Login;
 import mx.com.desoft.hidrogas.MainApp;
 
 public class LoginController {
+
 	@FXML
 	private Label userName;
+
 	@FXML
 	private Label password;
 
 	@FXML
 	private TextField userNameField;
+
 	@FXML
 	private TextField passordField;
 
 	@FXML
-	private void initialize() {
+	private Button iniciarSession;
 
+	@FXML
+	private void initialize() {
+		this.inicializarEventosBoton();
 	}
 
-	/**
-	 * Creates an empty address book.
-	 */
-	@FXML
 	private void iniciarSession() {
 		if	("admin".equals(userNameField.getText().toString()) && "admin".equals(passordField.getText().toString())){
 			MainApp app = new MainApp();
@@ -34,4 +42,19 @@ public class LoginController {
 		}
 	}
 
+	private void inicializarEventosBoton(){
+		iniciarSession.setOnKeyPressed(k ->{
+			final KeyCombination ENTER = new KeyCodeCombination(KeyCode.ENTER);
+	            if (ENTER.match(k)) {
+	            	iniciarSession();
+	            }
+		});
+
+		iniciarSession.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override
+		    public void handle(ActionEvent e) {
+		    	iniciarSession();
+		    }
+		});
+	}
 }
