15d6003e2830780364ad7d6d4ba4136d1f36aaac
DeiVeloper <david.c13@hotmail.com>
2017-09-12 07:09:54 -0500

    Cambios de validaciones agregar partes

--- src/mx/com/desoft/hidrogas/view/SeguimientoOrdenTrabajoController.java
+++ src/mx/com/desoft/hidrogas/view/SeguimientoOrdenTrabajoController.java
@@ -7,10 +7,12 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -19,6 +21,7 @@ import javafx.stage.Stage;
 import mx.com.desoft.hidrogas.MainApp;
 import mx.com.desoft.hidrogas.dto.OrdenTrabajoDTO;
 import mx.com.desoft.hidrogas.property.SeguimientoOrdenPartesProperty;
+import mx.com.desoft.hidrogas.util.Constantes;
 
 public class SeguimientoOrdenTrabajoController {
 
@@ -101,29 +104,82 @@ public class SeguimientoOrdenTrabajoController {
 	}
 
 	public void agregarPartesUsadas() {
-		dtoPartesUsadas.add(new SeguimientoOrdenPartesProperty(new SimpleStringProperty(cantidadPU.getText()), new SimpleStringProperty(noPU.getText()),
-				new SimpleStringProperty(marcaPU.getText()), new SimpleStringProperty(descripcionPU.getText()), new Button("Eliminar")));
-		dataPartesUsadas.clear();
-    	dataPartesUsadas.addAll(dtoPartesUsadas);
-		tablaPartesUsadas.setItems(getDataPartesUsadas());
-    	cantidadUsadaColumn.setCellValueFactory(cellData -> cellData.getValue().getCantidad());
-    	parteUsadaColumn.setCellValueFactory(cellData -> cellData.getValue().getParte());
-    	marcaUsadaColumn.setCellValueFactory(cellData -> cellData.getValue().getMarca());
-    	descripcionUsadaColumn.setCellValueFactory(cellData -> cellData.getValue().getDescripcion());
-    	eliminarUsadaColumn.setCellValueFactory(new PropertyValueFactory<SeguimientoOrdenPartesProperty, String> ("Eliminar"));
+		if(this.validarAgregarPartes((byte)1)) {
+			dtoPartesUsadas.add(new SeguimientoOrdenPartesProperty(new SimpleStringProperty(cantidadPU.getText()), new SimpleStringProperty(noPU.getText()),
+					new SimpleStringProperty(marcaPU.getText()), new SimpleStringProperty(descripcionPU.getText()), new Button("Eliminar")));
+			dataPartesUsadas.clear();
+	    	dataPartesUsadas.addAll(dtoPartesUsadas);
+			tablaPartesUsadas.setItems(getDataPartesUsadas());
+	    	cantidadUsadaColumn.setCellValueFactory(cellData -> cellData.getValue().getCantidad());
+	    	parteUsadaColumn.setCellValueFactory(cellData -> cellData.getValue().getParte());
+	    	marcaUsadaColumn.setCellValueFactory(cellData -> cellData.getValue().getMarca());
+	    	descripcionUsadaColumn.setCellValueFactory(cellData -> cellData.getValue().getDescripcion());
+	    	eliminarUsadaColumn.setCellValueFactory(new PropertyValueFactory<SeguimientoOrdenPartesProperty, String> ("Eliminar"));
+		}
+
 	}
 
 	public void agregarPartesSolicitadas() {
-		dtoPartesSolicitadas.add(new SeguimientoOrdenPartesProperty(new SimpleStringProperty(cantidadPS.getText()), new SimpleStringProperty(marcaPS.getText()),
-				new SimpleStringProperty(descripcionPS.getText()), new Button("Eliminar")));
-		dataPartesSolicitadas.clear();
-    	dataPartesSolicitadas.addAll(dtoPartesSolicitadas);
-		tablaPartesSolicitadas.setItems(getDataPartesSolicitadas());
-    	cantidadSolicitadaColumn.setCellValueFactory(cellData -> cellData.getValue().getCantidad());
-    	marcaSolicitadaColumn.setCellValueFactory(cellData -> cellData.getValue().getMarca());
-    	descripcionSolicitadaColumn.setCellValueFactory(cellData -> cellData.getValue().getDescripcion());
-    	eliminarSolicitadaColumn.setCellValueFactory(new PropertyValueFactory<SeguimientoOrdenPartesProperty, String> ("Eliminar"));
+		if(this.validarAgregarPartes((byte)2)) {
+			dtoPartesSolicitadas.add(new SeguimientoOrdenPartesProperty(new SimpleStringProperty(cantidadPS.getText()), new SimpleStringProperty(marcaPS.getText()),
+					new SimpleStringProperty(descripcionPS.getText()), new Button("Eliminar")));
+			dataPartesSolicitadas.clear();
+	    	dataPartesSolicitadas.addAll(dtoPartesSolicitadas);
+			tablaPartesSolicitadas.setItems(getDataPartesSolicitadas());
+	    	cantidadSolicitadaColumn.setCellValueFactory(cellData -> cellData.getValue().getCantidad());
+	    	marcaSolicitadaColumn.setCellValueFactory(cellData -> cellData.getValue().getMarca());
+	    	descripcionSolicitadaColumn.setCellValueFactory(cellData -> cellData.getValue().getDescripcion());
+	    	eliminarSolicitadaColumn.setCellValueFactory(new PropertyValueFactory<SeguimientoOrdenPartesProperty, String> ("Eliminar"));
+		}
+	}
 
+	private boolean validarAgregarPartes(byte tipo) {
+		String errorMessage = "";
+		boolean esCorrecto = false;
+		switch (tipo){
+			case 1:
+				if(cantidadPU.getText().matches("[0-9]*")) {
+					if(cantidadPU.getText() == Constantes.NULL || cantidadPU.getText().length() == Constantes.CERO) {
+						errorMessage = "El campo Cantidad no puede ir vacío.";
+					} else if(noPU.getText() == Constantes.NULL || noPU.getText().length() == Constantes.CERO) {
+						errorMessage = "El campo No de parte no puede ir vacío.";
+					} else if(marcaPU.getText() == Constantes.NULL || marcaPU.getText().length() == Constantes.CERO) {
+						errorMessage = "El campo Marca no puede ir vacío.";
+					} else if(descripcionPU.getText() == Constantes.NULL || descripcionPU.getText().length() == Constantes.CERO) {
+						errorMessage = "El campo Descripción no puede ir vacío.";
+					} else {
+						esCorrecto = true;
+					}
+				} else {
+					errorMessage = "El campo Cantidad debe ser numérico.";
+				}
+				break;
+			case 2:
+				if(cantidadPS.getText().matches("[0-9]*")) {
+					if(cantidadPS.getText() == Constantes.NULL || cantidadPS.getText().length() == Constantes.CERO) {
+						errorMessage = "El campo Cantidad no puede ir vacío.";
+					} else if(marcaPS.getText() == Constantes.NULL || marcaPS.getText().length() == Constantes.CERO) {
+						errorMessage = "El campo Marca no puede ir vacío.";
+					} else if(descripcionPS.getText() == Constantes.NULL || descripcionPS.getText().length() == Constantes.CERO) {
+						errorMessage = "El campo Descripción no puede ir vacío.";
+					} else {
+						esCorrecto = true;
+					}
+				} else {
+					errorMessage = "El campo Cantidad debe ser numérico.";
+				}
+				break;
+			default:
+				break;
+		}
+		if(esCorrecto == false) {
+			Alert alert = new Alert(AlertType.WARNING);
+	    	alert.setTitle("Validando Formulario");
+	    	alert.setHeaderText(null);
+	    	alert.setContentText(errorMessage);
+	    	alert.showAndWait();
+		}
+		return esCorrecto;
 	}
 
 	/**
