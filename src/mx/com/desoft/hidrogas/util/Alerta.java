714175e01d84b594eb5c09ca01687e4dbf164548
DeiVeloper <david.c13@hotmail.com>
2017-10-13 18:11:06 -0500

    Update Alerta.java

--- src/mx/com/desoft/hidrogas/util/Alerta.java
+++ src/mx/com/desoft/hidrogas/util/Alerta.java
@@ -5,18 +5,14 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 
 public class Alerta {
-	
-	public Alerta(String titulo, String mensaje, AlertType type){
-		crearAlertaUsuario(titulo, mensaje, type);
-	}
-	
+
 	public static void crearAlertaUsuario(String titulo, String mensaje, AlertType type ) {
 		ButtonType aceptar = new ButtonType("Aceptar");
-		ButtonType cancelar = new ButtonType("Cancelar");
-		Alert alert = new Alert(type,mensaje, aceptar, cancelar);
+		Alert alert = new Alert(type,mensaje, aceptar);
     	alert.setTitle(titulo);
     	alert.setHeaderText(null);
     	alert.showAndWait();
 	}
 
 }
+
