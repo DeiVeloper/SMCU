6a9f28cf01e388984223c9a04f538f29a113a502
DeiVeloper <david.c13@hotmail.com>
2017-10-13 18:12:13 -0500

    Update Reportes.java

--- src/mx/com/desoft/hidrogas/util/Reportes.java
+++ src/mx/com/desoft/hidrogas/util/Reportes.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -12,70 +13,50 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.formula.functions.T;
 
 public class Reportes {
-	
+
+	private static final Logger log = Logger.getLogger(Reportes.class);
+
 	public void generarReporteIncidencias(List<T> lista){
 		T[] miarray = new T[lista.size()];
         miarray = lista.toArray(miarray);
-		// First, it's needed to create the workbook
-				HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet    sheet    = workbook.createSheet("My Sheet");
 
-				// Then, sheets are created in the workbook
-				HSSFSheet    sheet    = workbook.createSheet("My Sheet");
+		for (int i = 0; i < miarray.length; i++) {
+			T registros =  miarray[i];
+			HSSFRow      row      = sheet.createRow(i);
+			HSSFCell     cell = row.createCell(i);
+			cell.setCellValue(registros.toString());
+		}
 
-				for (int i = 0; i < miarray.length; i++) {
-					T registros =  miarray[i];
-					HSSFRow      row      = sheet.createRow(i);
-					HSSFCell     cell = row.createCell(i);
-					cell.setCellValue(registros.toString());
-				}
-					
-				// Rows are created in the sheets
-
-				// Last, every row is composed by cells
-				
-
-				// Writing the excel to output file
-				try {
-					OutputStream out = new FileOutputStream("src/main/resources/SimpleExcel.xls");
-					workbook.write(out);
-					workbook.close();
-					out.flush();
-					out.close();
-				}
-				catch (IOException e) {
-					System.err.println("Error at file writing");
-					e.printStackTrace();
-				}
+		try {
+			OutputStream out = new FileOutputStream("src/main/resources/SimpleExcel.xls");
+			workbook.write(out);
+			workbook.close();
+			out.flush();
+			out.close();
+		}
+		catch (IOException e) {
+			log.error("Error at file writing",e);
+		}
 	}
-	
+
 	public void generarReporteTipoReparacion(){
-		// First, it's needed to create the workbook
-				HSSFWorkbook workbook = new HSSFWorkbook();
-
-				// Then, sheets are created in the workbook
-				HSSFSheet    sheet    = workbook.createSheet("My Sheet");
-
-				// Rows are created in the sheets
-				HSSFRow      row      = sheet.createRow(0);
-
-				// Last, every row is composed by cells
-				HSSFCell     cell = row.createCell(0);
-				cell.setCellValue("Hello, World!");
-
-				// Writing the excel to output file
-				try {
-					OutputStream out = new FileOutputStream("src/main/resources/SimpleExcel.xls");
-					workbook.write(out);
-					workbook.close();
-					out.flush();
-					out.close();
-				}
-				catch (IOException e) {
-					System.err.println("Error at file writing");
-					e.printStackTrace();
-				}
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.createSheet("My Sheet");
+		HSSFRow row = sheet.createRow(0);
+		HSSFCell cell = row.createCell(0);
+		cell.setCellValue("Hello, World!");
+		try {
+			OutputStream out = new FileOutputStream("SimpleExcel.xls");
+			workbook.write(out);
+			workbook.close();
+			out.flush();
+			out.close();
+		}
+		catch (IOException e) {
+			log.error("Error at file writing",e);
+		}
 	}
-
-	
 
 }
