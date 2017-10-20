e874708288649eeb75f102cc0c89d37ae4f10a37
DeiVeloper <david.c13@hotmail.com>
2017-10-20 14:10:33 -0500

    Add files via upload

--- src/mx/com/desoft/hidrogas/buttons/ButtonCerrarOrden.java
+++ src/mx/com/desoft/hidrogas/buttons/ButtonCerrarOrden.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TableCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import mx.com.desoft.hidrogas.property.OrdenProperty;
+import mx.com.desoft.hidrogas.util.Constantes;
 import mx.com.desoft.hidrogas.view.AdministrarOrdenTrabajoController;
 
 public class ButtonCerrarOrden extends TableCell<OrdenProperty, Boolean> {
@@ -18,28 +19,35 @@ public class ButtonCerrarOrden extends TableCell<OrdenProperty, Boolean> {
 	final Image imageEliminar = new Image("file:resources/images/cerrar.png");
 	final Button cellButton = new Button("", new ImageView(imageEliminar));
 	private AdministrarOrdenTrabajoController administrarOrdenController;
+	private OrdenProperty orden;
 
 	public ButtonCerrarOrden(ObservableList<OrdenProperty> dataOrdenes, List<OrdenProperty> dtoPartesOrdenes) {
-
-		// Action when the button is pressed
 		cellButton.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent t) {
 				administrarOrdenController = new AdministrarOrdenTrabajoController();
 				// get Selected Item
-				OrdenProperty orden = (OrdenProperty) ButtonCerrarOrden.this.getTableView().getItems().get(ButtonCerrarOrden.this.getIndex());
-				administrarOrdenController.cerrarOrden(Integer.parseInt(orden.getFolioOrden().getValue()));
+				orden = (OrdenProperty) ButtonCerrarOrden.this.getTableView().getItems().get(ButtonCerrarOrden.this.getIndex());
+				boolean cerrado = administrarOrdenController.cerrarOrden(Integer.parseInt(orden.getFolioOrden().getValue()));
+				if(cerrado){
+					cellButton.setVisible(false);
+				}
+				dtoPartesOrdenes.set(ButtonCerrarOrden.this.getIndex(), orden);
 			}
 		});
 	}
 
 	// Display button if the row is not empty
-	@Override
 	protected void updateItem(Boolean t, boolean empty) {
 		super.updateItem(t, empty);
 		if (!empty) {
-			setGraphic(cellButton);
+			orden = (OrdenProperty) ButtonCerrarOrden.this.getTableView().getItems().get(getIndex());
+			if	(orden.getEstatus() == Constantes.ESTATUS_ORDEN_CERRADA){
+				setGraphic(null);
+			} else	{
+				setGraphic(cellButton);
+			}
 		} else {
 			setGraphic(null);
 		}
