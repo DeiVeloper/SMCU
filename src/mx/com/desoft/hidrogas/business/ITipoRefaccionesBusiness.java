c992c19862b42788cb71f579d8f2ff2ddb847292
DeiVeloper <david.c13@hotmail.com>
2017-10-31 16:11:09 -0600

    Add files via upload

--- src/mx/com/desoft/hidrogas/business/ITipoRefaccionesBusiness.java
+++ src/mx/com/desoft/hidrogas/business/ITipoRefaccionesBusiness.java
@@ -3,6 +3,8 @@ package mx.com.desoft.hidrogas.business;
 import java.io.Serializable;
 import java.util.List;
 
+import org.hibernate.exception.ConstraintViolationException;
+
 import mx.com.desoft.hidrogas.dto.CatTipoRefaccionesDTO;
 import mx.com.desoft.hidrogas.property.TipoRefaccionProperty;
 
@@ -13,5 +15,11 @@ public interface ITipoRefaccionesBusiness extends Serializable {
 	List<TipoRefaccionProperty> getTipoRefaccionByView(CatTipoRefaccionesDTO catTipoRefaccionesDTO);
 
 	void eliminarTipoRefaccionById(Integer id);
+
+	void guardarRefaccion(CatTipoRefaccionesDTO catTipoRefaccionesDTO);
+
+	List<TipoRefaccionProperty> getRefaccionByView(CatTipoRefaccionesDTO catTipoRefaccionesDTO);
+
+	void eliminarRefaccionById(Integer id) throws ConstraintViolationException;
 
 }
--- src/mx/com/desoft/hidrogas/business/TipoRefaccionesBusinessImpl.java
+++ src/mx/com/desoft/hidrogas/business/TipoRefaccionesBusinessImpl.java
@@ -1,15 +1,20 @@
 package mx.com.desoft.hidrogas.business;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javafx.beans.property.SimpleStringProperty;
+import mx.com.desoft.hidrogas.Authenticator;
 import mx.com.desoft.hidrogas.dao.CatTipoRefaccionesDAO;
+import mx.com.desoft.hidrogas.dao.ICatTipoRefaccionDAO;
 import mx.com.desoft.hidrogas.dto.CatTipoRefaccionesDTO;
 import mx.com.desoft.hidrogas.model.CatTipoListaRefaccion;
+import mx.com.desoft.hidrogas.model.CatTipoRefaccion;
 import mx.com.desoft.hidrogas.property.TipoRefaccionProperty;
 
 @Service
@@ -22,6 +27,9 @@ public class TipoRefaccionesBusinessImpl implements ITipoRefaccionesBusiness {
 
 	@Autowired
 	private CatTipoRefaccionesDAO catTipoRefaccionesImplDAO;
+
+	@Autowired
+	private ICatTipoRefaccionDAO catTipoRefaccionImplDAO;
 
 	@Override
 	public void guardarTipoRefaccion(CatTipoRefaccionesDTO catTipoRefaccionesDTO) {
@@ -47,6 +55,33 @@ public class TipoRefaccionesBusinessImpl implements ITipoRefaccionesBusiness {
 	@Override
 	public void eliminarTipoRefaccionById(Integer id) {
 		catTipoRefaccionesImplDAO.delete(id);
+	}
+
+	@Override
+	public void guardarRefaccion(CatTipoRefaccionesDTO catTipoRefaccionesDTO) {
+		catTipoRefaccionImplDAO.saveOrUpdate(this.convertirRefaccionDTOToEntidad(catTipoRefaccionesDTO));
+
+	}
+
+	@Override
+	public List<TipoRefaccionProperty> getRefaccionByView(CatTipoRefaccionesDTO catTipoRefaccionesDTO) {
+		List<TipoRefaccionProperty> listaDTO = new ArrayList<>();
+		for (CatTipoRefaccion lts : catTipoRefaccionImplDAO.getTipoRefaccionByView(catTipoRefaccionesDTO)) {
+			TipoRefaccionProperty dto = new TipoRefaccionProperty(new SimpleStringProperty(String.valueOf(lts.getIdTipoRefaccion())),
+					new SimpleStringProperty(lts.getDescripcion()));
+			listaDTO.add(dto);
+		}
+		return listaDTO;
+	}
+
+	@Override
+	public void eliminarRefaccionById(Integer id) throws ConstraintViolationException {
+		catTipoRefaccionImplDAO.delete(id);
+
+	}
+
+	private CatTipoRefaccion convertirRefaccionDTOToEntidad(CatTipoRefaccionesDTO catTipoRefaccionesDTO) {
+		return new CatTipoRefaccion(catTipoRefaccionesDTO.getDescripcion(), new Date(), Authenticator.usuarioSesion.getNominaEmpleado());
 	}
 
 }
