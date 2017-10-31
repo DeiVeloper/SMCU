d352bfecd9ffb8fb81a404aa752077e4b9d269dc
DeiVeloper <david.c13@hotmail.com>
2017-10-31 16:11:35 -0600

    Add files via upload

--- src/mx/com/desoft/hidrogas/dao/CatTipoRefaccionImplDAO.java
+++ src/mx/com/desoft/hidrogas/dao/CatTipoRefaccionImplDAO.java
@@ -1,11 +1,30 @@
 package mx.com.desoft.hidrogas.dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import mx.com.desoft.hidrogas.dto.CatTipoRefaccionesDTO;
 import mx.com.desoft.hidrogas.hibernate.HibernateImplDAO;
 import mx.com.desoft.hidrogas.model.CatTipoRefaccion;
+import mx.com.desoft.hidrogas.model.HibernateUtil;
+import mx.com.desoft.hidrogas.util.Constantes;
 
 @Repository
 public class CatTipoRefaccionImplDAO extends HibernateImplDAO<CatTipoRefaccion, Integer> implements ICatTipoRefaccionDAO {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<CatTipoRefaccion> getTipoRefaccionByView(CatTipoRefaccionesDTO catTipoRefaccionesDTO) {
+		Criteria criteria = HibernateUtil.openSession().createCriteria(CatTipoRefaccion.class);
+		if(catTipoRefaccionesDTO.getDescripcion() != Constantes.NULL && !catTipoRefaccionesDTO.getDescripcion().isEmpty()){
+			criteria.add(Restrictions.like("descripcion", catTipoRefaccionesDTO.getDescripcion()));
+		}
+		criteria.addOrder(Order.asc("idTipoRefaccion"));
+		return (List<CatTipoRefaccion>) criteria.list();
+	}
 
 }
--- src/mx/com/desoft/hidrogas/dao/ICatTipoRefaccionDAO.java
+++ src/mx/com/desoft/hidrogas/dao/ICatTipoRefaccionDAO.java
@@ -1,8 +1,12 @@
 package mx.com.desoft.hidrogas.dao;
 
+import java.util.List;
+
+import mx.com.desoft.hidrogas.dto.CatTipoRefaccionesDTO;
 import mx.com.desoft.hidrogas.hibernate.HibernateDAO;
 import mx.com.desoft.hidrogas.model.CatTipoRefaccion;
 
 public interface ICatTipoRefaccionDAO extends HibernateDAO<CatTipoRefaccion, Integer> {
 
+	List<CatTipoRefaccion> getTipoRefaccionByView(CatTipoRefaccionesDTO catTipoRefaccionesDTO);
 }
